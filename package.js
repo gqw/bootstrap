2d9e291fb018314c8d9ef8d63db5451604f1a7b9
顾起威 <guqiwei@corp.netease.com>
2020-08-13 20:06:18 +0800

    for gitee
    Signed-off-by: 顾起威 <guqiwei@corp.netease.com>

--- package.js
+++ package.js
@@ -1,9 +1,11 @@
 // package metadata file for Meteor.js
 
+/* eslint-env meteor */
+
 Package.describe({
-  name: 'twbs:bootstrap', // https://atmospherejs.com/twbs/bootstrap
+  name: 'twbs:bootstrap',  // https://atmospherejs.com/twbs/bootstrap
   summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
-  version: '4.5.2',
+  version: '4.1.3',
   git: 'https://github.com/twbs/bootstrap.git'
 });
 
